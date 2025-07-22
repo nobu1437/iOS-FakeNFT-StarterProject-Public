646b7b983425d7a87d19e7f0c72b3167f64bf396
IlyaAnufriev <ilya.anufriev2014@yandex.ru>
2025-07-22 10:24:21 +0300

    Обновлен CurrencyService и добавлен запрос на оплату

--- FakeNFT/Services/Currency/Protocols /CurrencyServiceProtocol.swift
+++ FakeNFT/Services/Currency/Protocols /CurrencyServiceProtocol.swift
@@ -2,5 +2,6 @@ import Foundation
 
 protocol CurrencyServiceProtocol {
     func getCurrencies(onResponse: @escaping(Result<[CurrencyModel],Error>) -> Void)
+    func pay(currencyId: String, onResponse: @escaping(Result<PaymentDTO, any Error>) -> Void)
 }
 
